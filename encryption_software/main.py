4580516c80d14c148700b12da80c033e89922438
beny201 <artur.wudz@gmail.com>
2023-05-16 10:39:23 +0200

    feat: Initialize project structure

---
+++ encryption_software/main.py
@@ -0,0 +1,8 @@
+from manager_class import *
+
+def main():
+    manger = Manager()
+    manger.run()
+
+if __name__ == "__main__":
+    main()
---
+++ encryption_software/manager_class.py
@@ -0,0 +1,6 @@
+class Manager:
+    def __init__(self):
+
+
+    def run(self):
+        pass
