83e9b1128672f49bdbe8a9dd15803a5495e607c5
beny201 <artur.wudz@gmail.com>
2023-05-20 19:22:40 +0200

    refactor: rename folder with tests

--- tests/test_encryption.py
+++ tests/test_encryption.py
@@ -1,11 +1,11 @@
 import pytest
-from encryption_software.encryption import *
+from functionality.encryption import *
 
 
 @pytest.mark.parametrize("txt, expected", [("a", "n"), ("T", "G")])
 def test_should_return_char_for_rot13c(txt, expected):
-    text_object = Text(txt)
-    assert text_object.rot13c(txt) == expected
+    text_object = Rot13(txt)
+    assert text_object.rot_c(txt) == expected
 
 
 @pytest.mark.parametrize("txt, expected", [(
@@ -14,14 +14,14 @@ def test_should_return_char_for_rot13c(txt, expected):
         "guvf vf purpx, dhrfgvba?",
         "this is check, question?")])
 def test_should_return_sentence_for_rot13(txt, expected):
-    text_object = Text(txt)
-    assert text_object.rot13() == expected
+    text_object = Rot13(txt)
+    assert text_object.rot() == expected
 
 
 @pytest.mark.parametrize("txt, expected", [("!", "P"), ("~", "O"), ("5", "d")])
 def test_should_return_char_for_rot47c(txt, expected):
-    text_object = Text(txt)
-    assert text_object.rot47c(txt) == expected
+    text_object = Rot47(txt)
+    assert text_object.rot_c(txt) == expected
 
 
 @pytest.mark.parametrize("txt, expected", [(
@@ -29,5 +29,5 @@ def test_should_return_char_for_rot47c(txt, expected):
     ("%@ K52?:6 ;6DE K2<@5@H2?6]",
      "To zdanie jest zakodowane.")])
 def test_should_return_sentence_for_rot47(txt, expected):
-    text_object = Text(txt)
-    assert text_object.rot47() == expected
+    text_object = Rot47(txt)
+    assert text_object.rot() == expected
