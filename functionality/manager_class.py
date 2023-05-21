cda80fe5dcfb7fbcb94c1ce422150c7f0203c888
beny201 <artur.wudz@gmail.com>
2023-05-21 10:10:16 +0200

    feat: Add method for user action validation

--- functionality/manager_class.py
+++ functionality/manager_class.py
@@ -21,11 +21,6 @@ class Manager:
             6: self.finish
         }
 
-        self.action_submenu = {
-            1: self.rot13,
-            2: self.rot47,
-        }
-
     def run(self) -> None:
         while self.working:
             self.menu.show()
@@ -54,24 +49,43 @@ class Manager:
 
     def process_encryption(self):
         self.menu.show_submenu()
-        user_choice = self.user_choice_action()
+        user_choice = self.user_choice_action_validation_sub_menu()
         user_message_to_encrypt = self.user_message()
+        message_to_stock = self.encryption(user_choice,
+                                           user_message_to_encrypt)
+        self.buffer.append_text(message_to_stock)
+
+    def user_choice_action_validation_sub_menu(self):
+        check_on = True
+        while check_on:
+            scope = len(self.menu.actions_submenu) + 1
+            try:
+                user_choice = self.user_choice_action()
+                if user_choice >= scope or user_choice <= 0:
+                    raise ValueError()
+                else:
+                    check_on = False
+                    return user_choice
+            except ValueError:
+                print((f"This need to be from 1 to {scope}"))
+
+    def encryption(self, user_choice, message_to_encrypt):
         if user_choice == 1:
-            encrypted_message = Rot13(user_message_to_encrypt).rot()
+            encrypted_message = Rot13(message_to_encrypt).rot()
             message_to_stock = Text(encrypted_message, "Rot13", "Encrypted")
             print(f"Encrypted: {encrypted_message}/ This will be stocked")
-            self.buffer.append_text(message_to_stock)
+            return message_to_stock
         elif user_choice == 2:
-            encrypted_message = Rot47(user_message_to_encrypt).rot()
+            encrypted_message = Rot47(message_to_encrypt).rot()
             message_to_stock = Text(encrypted_message, "Rot47", "Encrypted")
-            self.buffer.append_text(message_to_stock)
+            print(f"Encrypted: {encrypted_message}/ This will be stocked")
+            return message_to_stock
         else:
-            print(f"Please chose no from range 1 - {len(self.action_submenu)}")
-
+            print(
+                f"Please chose no from range 1 - {len(self.menu.actions_submenu)}")
 
     def process_decryption(self):
         pass
-
 
     def execute_main(self, user_choice: int):
         if user_choice in self.action_main.keys():
@@ -85,27 +99,26 @@ class Manager:
         print(files_to_save)
         saving_file(name_file, files_to_save)
 
-
     def reading_file_manager(self):
         pass
-
-
 
     def finish(self):
         self.working = False
 
+
 def main():
     obiekt1 = Manager()
     obiekt1.run()
-    #obiekt3 = Text("asdasd")
-    #obiekt1.buffer.append_text(obiekt3)
-    #zapis = obiekt1.buffer.data_for_saving()
+    # obiekt3 = Text("asdasd")
+    # obiekt1.buffer.append_text(obiekt3)
+    # zapis = obiekt1.buffer.data_for_saving()
 
-    #obiekt1.procces_encrytpion()
-    #obiekt1.buffer.read_contents()
-    #saving_file("marian", *zapis)
-    #data = reading_file("marian")
-    #print(data)
+    # obiekt1.procces_encrytpion()
+    # obiekt1.buffer.read_contents()
+    # saving_file("marian", *zapis)
+    # data = reading_file("marian")
+    # print(data)
+
 
 if __name__ == "__main__":
     main()
