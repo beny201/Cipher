108a4742d176c8738e4110439638b1378d24e4bf
beny201 <artur.wudz@gmail.com>
2023-05-22 19:59:03 +0200

    refactor: add new position to menu

--- functionality/menu_class.py
+++ functionality/menu_class.py
@@ -4,14 +4,15 @@ class Menu:
             1: "Encryption",
             2: "Decryption",
             3: "Show all messages",
-            4: "Save messages to file",
-            5: "Open saved file",
-            6: "Close program"
+            4: "Remove message from stock",
+            5: "Save messages to file",
+            6: "Open saved file",
+            7: "Close program"
         }
         self.actions_submenu = {
             1: "Rot 13",
             2: "Rot 47",
-        }
+         }
 
     def show(self) -> None:
         print("-" * 20)
@@ -26,3 +27,19 @@ class Menu:
         for key, action in self.actions_submenu.items():
             print(f"{key}. {action}")
         print("-" * 20)
+
+def main():
+    obiekt1 = Menu()
+    print(len(obiekt1.actions_submenu))
+    #obiekt3 = Text("asdasd")
+    #obiekt1.buffer.append_text(obiekt3)
+    #zapis = obiekt1.buffer.data_for_saving()
+
+    #obiekt1.procces_encrytpion()
+    #obiekt1.buffer.read_contents()
+    #saving_file("marian", *zapis)
+    #data = reading_file("marian")
+    #print(data)
+
+if __name__ == "__main__":
+    main()
