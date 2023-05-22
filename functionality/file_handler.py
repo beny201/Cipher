a2767952e268394dac3723e4a8202d1ae742100a
beny201 <artur.wudz@gmail.com>
2023-05-22 18:13:38 +0200

    refactor: saving file to separate folder

--- functionality/file_handler.py
+++ functionality/file_handler.py
@@ -1,23 +1,26 @@
 import json
+import os.path
 from typing import List
 
 
 def saving_file(name: str, data: List[dict]) -> None:
+    folder = "json_files"
+    file_path = os.path.join(folder, name)
     try:
-        with open(f"{name}.json", "r") as f:
+        with open(f"{file_path}.json", "r") as f:
             file_exist = True
     except FileNotFoundError:
         file_exist = False
     try:
         if file_exist:
-            with open(f"{name}.json", "r+") as json_file:
+            with open(f"{file_path}.json", "r+") as json_file:
                 existing_data = json.load(json_file)
                 for files in data:
                     existing_data.append(files)
-            with open(f"{name}.json", "w") as json_file:
+            with open(f"{file_path}.json", "w") as json_file:
                 json.dump(existing_data, json_file, indent=4)
         else:
-            with open(f"{name}.json", "a") as json_file:
+            with open(f"{file_path}.json", "a") as json_file:
                 data_to_save = data
                 json.dump(data_to_save, json_file, indent=4)
     except FileNotFoundError:
@@ -27,7 +30,9 @@ def saving_file(name: str, data: List[dict]) -> None:
 # jak to zrobic na dictcie? albo dodac nazwe  Nie udalo mi sie dodac dicta,
 # wiec jest lista
 def reading_file(name: str) -> List[dict]:
-    with open(f"{name}.json", "r") as json_file:
+    folder = "json_files"
+    file_path = os.path.join(folder, name)
+    with open(f"{file_path}.json", "r") as json_file:
         data = json.load(json_file)
         return data
 
