c3c3e8c7ff5d2ed8ff53ab4f93818f257f66400a
beny201 <artur.wudz@gmail.com>
2023-05-25 20:51:31 +0200

    feat: add tests for buffer module

---
+++ tests/test_buffer.py
@@ -0,0 +1,51 @@
+import pytest
+from functionality.buffer import *
+import functionality.buffer
+from dataclasses import asdict
+
+@pytest.fixture
+def set_object():
+    buffer = Buffer()
+    text_object = Text()
+    buffer.append_text(text_object)
+    return buffer, text_object
+
+
+def test_should_return_dic(set_object):
+    buffer, text_object = set_object
+    assert buffer.data_for_saving() == [asdict(text_object)]
+    buffer.append_text(text_object)
+    assert buffer.data_for_saving() == [asdict(text_object), asdict(
+        text_object)]
+
+
+def test_should_add_object_to_list(set_object):
+    buffer, text_object = set_object
+    assert len(buffer.temporary_container) == 1
+    assert text_object in buffer.temporary_container
+
+
+def test_should_remove_object_from_list(set_object):
+    buffer, text_object = set_object
+    position = 0
+    buffer.remove_message(position)
+    assert len(buffer.temporary_container) == 0
+    assert text_object not in buffer.temporary_container
+
+def test_should_return_int_from_user_action(mocker):
+    buffer = Buffer()
+    text_object = Text()
+    buffer.append_text(text_object)
+    mocker.patch('builtins.input', return_value="1")
+    assert buffer.user_action() == 0
+
+
+"""def test_raise_error_when_not_int(mocker):
+    buffer = Buffer()
+    text_object = Text()
+    buffer.append_text(text_object)
+    mocker.patch('builtins.input', return_value="2")
+    with pytest.raises(ValueError):
+        buffer.user_action()
+"""
+# do sprawdzenia czego nie bangla
